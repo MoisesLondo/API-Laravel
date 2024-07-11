5e74a7f2a09bf5d30c8df8fa634c5da3c615e2aa
MoisesLondo <moiseslondo16@gmail.com>
2024-07-11 01:04:12 -0400

    xd

--- laravel-api/app/Http/Controllers/bookController.php
+++ laravel-api/app/Http/Controllers/bookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\Book;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class bookController extends Controller
@@ -13,5 +14,18 @@ class bookController extends Controller
             'status'=> 200
         ];
         return response()->json($data,200);
+    }
+
+    public function save(Request $request){
+        $validator = Validator::make($request->all(),[
+            'title' => 'required',
+            'author' => 'required',
+            'birth_year' => 'required',
+            'death_year' => 'required',
+            'language' => 'required',
+            'genres' => 'required',
+            'pdf_path' => 'required',
+            'image_path' => 'required',
+        ]);
     }
 }
