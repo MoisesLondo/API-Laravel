2fc41f7dd7f4e7397eec9d3a1bc7eff67638d6fd
MoisesLondo <moiseslondo16@gmail.com>
2024-07-10 23:48:42 -0400

    modelo

---
+++ laravel-api/app/Models/Book.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $table = 'books';
+
+    protected $fillable = [
+        'title',
+        'author',
+        'birth_year',
+        'death_year',
+        'language',
+        'genres',
+        'pdf_path',
+        'image_path'
+
+    ];
+}
