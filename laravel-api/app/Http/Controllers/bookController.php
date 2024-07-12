f6f807800c66c0918228426209500610abc12089
MoisesLondo <moiseslondo16@gmail.com>
2024-07-11 23:08:21 -0400

    update, oneBook y delete

--- laravel-api/app/Http/Controllers/bookController.php
+++ laravel-api/app/Http/Controllers/bookController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class bookController extends Controller
 {
-    public function index(){
+    public function books(){
         $books = Book::all();
         $data = [
             'books' => $books,
@@ -64,4 +64,89 @@ class bookController extends Controller
         ];
         return response()->json($data,201);
     }
+
+    public function oneBook($id){
+        $book = Book::find($id);
+        if(!$book){
+            $data = [
+                'message' => 'El libro no existe',
+                'status' => 404
+            ];
+            return response()->json($data,404);
+
+        }
+        $data = [
+            'book' => $book,
+            'status' => 200
+        ];
+        return response()->json($data,200);
+    }
+
+    public function delete($id){
+        $book = Book::find($id);
+        if(!$book){
+            $data = [
+                'message' => 'El libro no existe',
+                'status' => 404
+            ];
+            return response()->json($data,404);
+
+        }
+        $book->delete();
+
+        $data = [
+            'message' => 'Libro eliminado',
+            'status' => 200
+        ];
+        return response()->json($data,200);
+    }
+
+    public function update(Request $request, $id){
+        $book = Book::find($id);
+        if(!$book){
+            $data = [
+                'message' => 'El libro no existe',
+                'status' => 404
+            ];
+            return response()->json($data,404);
+
+        }
+        $validator = Validator::make($request->all(),[
+            'title' => 'required',
+            'author' => 'required',
+            'birth_year' => 'required',
+            'death_year' => 'required',
+            'language' => 'required',
+            'genres' => 'required',
+            'pdf_path' => 'required',
+            'image_path' => 'required',
+        ]);
+        if($validator->fails()){
+            $data = [
+                'message' => 'Error en los datos',
+                'errors' => $validator->errors(),
+                'status' => 400
+
+            ];
+            return response()->json($data,400);
+        }
+        $book->title = $request->title;
+        $book->author = $request->author;
+        $book->birth_year = $request->birth_year;
+        $book->death_year = $request->death_year;
+        $book->language = $request->language;
+        $book->genres = $request->genres;
+        $book->pdf_path = $request->pdf_path;
+        $book->image_path = $request->image_path;
+
+        $book->save();
+        $data = [
+            'message' => 'Libro actualizado',
+            'book' => $book,
+            'status' => 200
+            ];
+            return response()->json($data,200);
+
+    }
+
 }
--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -4,18 +4,12 @@ use App\Http\Controllers\bookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/books', [bookController::class, 'index']);
+Route::get('/books', [bookController::class, 'books']);
 
-Route::get('/books/{id}', function(){
-    return "libro";
-});
+Route::get('/books/{id}',[bookController::class, 'oneBook']);
 
 Route::post('/books', [bookController::class, 'save']);
 
-Route::put('/books/{id}', function(){
-    return "actualizando libro";
-});
+Route::put('/books/{id}', [bookController::class, 'update']);
 
-Route::delete('/books/{id}', function(){
-    return "eliminando libros";
-});
+Route::delete('/books/{id}', [bookController::class, 'delete']);
